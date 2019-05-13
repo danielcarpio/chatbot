bac835f1cb53a2abac2d82a80f7352668710442c
raffrearaUS <raffrearaus@gmail.com>
2019-05-13 09:20:51 +0200

    Incluido JSON

--- tree-generator/main/views.py
+++ tree-generator/main/views.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from pandas import read_json
 from pandas import DataFrame
 from django.shortcuts import HttpResponse
+import re
 
 class Tree(APIView):
     def get(self, request):
@@ -38,6 +39,44 @@ class Tree(APIView):
                 res = res.drop(i[0])
         return res
 
+    def transformar_json(self, lineas, label_encoders, prof, col):
+        res = {
+            'atributo': None,
+            'ramas': []
+        }
+        for i in range(len(lineas)):
+            fin_lineas = 4 + 4 * prof
+            if lineas[i][0:fin_lineas] == prof * '|   ' + '|---':
+                num_feature = re.findall('feature_\d+', lineas[i])[0][8:]
+                atrib = list(label_encoders.keys())[int(num_feature)]
+                if res['atributo'] is None:
+                    res['atributo'] = atrib
+                clasif = int(lineas[i][-4:-3])
+                orden = range(clasif + 1)
+                if lineas[i][-7:-6] == '>':
+                    clasif += 1
+                    orden = range(clasif, len(label_encoders[atrib].classes_))
+                for k in orden:
+                    valor = label_encoders[atrib].inverse_transform([k])[0]
+                    if 'class' in lineas[i + 1]:
+                        num_class = re.findall('class: \d+', lineas[i + 1])[0][7:]
+                        clase = label_encoders[col].inverse_transform([int(num_class)])[0]
+                        res['ramas'].append({
+                            'valor': valor,
+                            'siguiente': clase
+                        })
+                    else:
+                        fin_rama = len(lineas)
+                        for j in range(len(lineas[i + 1:])):
+                            if lineas[i + 1 + j][0:fin_lineas] == prof * '|   ' + '|---':
+                                fin_rama = i + 1 + j
+                                break
+                        res['ramas'].append({
+                            'valor': valor,
+                            'siguiente': self.transformar_json(lineas[i + 1:fin_rama], label_encoders, prof + 1, col)
+                        })
+        return res
+
     def post(self, request):
         file = request.FILES['archivo']
         col = request.POST.get('columna_objetivo')
@@ -58,10 +97,10 @@ class Tree(APIView):
         if not col in data.columns.values:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        if max_depth is None or max_depth == 0 or max_depth < 0:
+        if max_depth is None or int(max_depth) == 0 or int(max_depth) < 0:
             clf = tree.DecisionTreeClassifier()
         else:
-            clf = tree.DecisionTreeClassifier(max_depth=max_depth)
+            clf = tree.DecisionTreeClassifier(max_depth=int(max_depth))
 
         labels_encoders = {}
         for d in data:
@@ -69,12 +108,11 @@ class Tree(APIView):
             le.fit(data[d])
             labels_encoders[d] = le
             data[d] = le.transform(data[d])
-        
         x = DataFrame.copy(data).drop(col, axis=1)
         y = DataFrame.copy(data).drop(data.columns.difference([col]), axis=1)
 
         clf.fit(x, y)
 
         tree_text = export_text(clf)
-        
-        return Response({'tree': tree_text}, status=status.HTTP_200_OK)
+        jsonres = self.transformar_json(tree_text.split('\n'), labels_encoders, 0, col)
+        return Response(jsonres, status=status.HTTP_200_OK)
